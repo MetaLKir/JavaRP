e51b734ffa6e96effbd516557c02ffd4ea749745
MetaL Kir <masterzatk@gmail.com>
2025-05-21 00:17:29 +0300

    Lesson 8 homework

--- hw_08_QuadraticEquation/src/telran/equation/controller/EquationAppl.java
+++ hw_08_QuadraticEquation/src/telran/equation/controller/EquationAppl.java
@@ -4,18 +4,14 @@ import telran.equation.model.QuadraticEquation;
 
 public class EquationAppl {
     public static void main(String[] args) {
-        QuadraticEquation eq = new QuadraticEquation(2,-4, -6);
-        // Descriminant = b^2 - 4ac = 16 + 48 = 64
-        // x1 = (-b + koren'Desc) / 2a = (4+8) / 4 = 3
-        // x2 = (4-8) / 4 = -1
-        eq.display();
-        System.out.println("Delta = " +eq.delta());
-        System.out.println("Number of roots: " + eq.quantityRoots());
+        QuadraticEquation eq = new QuadraticEquation(2, -4, -6);
+        eq.displayEquation();
+        System.out.println("Delta = " + eq.getDelta());
+        System.out.println("Number of roots = " + eq.getQuantityRoots());
 
         double[] roots = eq.getRoots();
         for (int i = 0; i < roots.length; i++) {
-
+            System.out.println("root[" + (i + 1) + "] = " + roots[i]);
         }
     }
-
 }
--- hw_08_QuadraticEquation/src/telran/equation/model/QuadraticEquation.java
+++ hw_08_QuadraticEquation/src/telran/equation/model/QuadraticEquation.java
@@ -4,55 +4,84 @@ public class QuadraticEquation {
     private double a;
     private double b;
     private double c;
+    private double delta;
+    private double[] roots;
 
-    // ax^2 + bx + c = 0
-    // b^2 - 4ac = 16
-
-
-    public double getA() {
-        return a;
-    }
-
-    public void setA(double a) {
-        this.a = a;
-    }
-
-    public double getB() {
-        return b;
-    }
-
-    public void setB(double b) {
-        this.b = b;
-    }
-
-    public double getC() {
-        return c;
-    }
-
-    public void setC(double c) {
-        this.c = c;
-    }
-
+    // CONSTRUCTOR
     public QuadraticEquation(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
+        solveEquation();
     }
 
-    public void display(){
-        // TODO: ax^2 + bx + c = 0
+    // GETTERS==========================================
+    public double getA() { return a; }
+    public double getB() { return b; }
+    public double getC() { return c; }
+    public double getDelta() { return delta; }
+    public double[] getRoots() { return roots; }
+
+    public int getQuantityRoots() {
+        if(delta >= 0)
+            return roots.length;
+        else
+            return 0;
     }
 
-    public String delta() {
-        return "";
+    // SETTERS==========================================
+    public void setA(double a) {
+        this.a = a;
+        solveEquation();
     }
 
-    public String quantityRoots() {
-        // TODO: if delta < 0 -> 0 roots
-        return "";
+    public void setB(double b) {
+        this.b = b;
+        solveEquation();
     }
 
-    public double[] getRoots() {
-        return new double[]{0.};
+    public void setC(double c) {
+        this.c = c;
+        solveEquation();
     }
+
+    // MATH STUFF==========================================
+    private void solveEquation(){
+        calculateDelta();
+        calculateRoots();
+    }
+
+    private void calculateDelta(){
+        delta = (b * b) - (4 * a * c);
+    }
+
+    private double calculateRootFirst() {
+        return (-b + Math.sqrt(delta)) / (2 * a);
+    }
+
+    private double calculateRootSecond() {
+        return (-b - Math.sqrt(delta)) / (2 * a);
+    }
+
+    private void calculateRoots(){
+        if (delta < 0)
+            roots = new double[]{0.};
+        else if (delta > 0)
+            roots = new double[]{calculateRootFirst(), calculateRootSecond()};
+        else if (delta == 0)
+            roots = new double[]{calculateRootFirst()};
+    }
+
+    // UTILITY==========================================
+    public void displayEquation(){
+        //TODO ax^2+bx+c=0
+        String toDisplay = "(" + a + " * x^2) + (" + b + " * x) + " + c + " = 0";
+        System.out.println(toDisplay);
+    }
+
+
+
+
+
+
 }
