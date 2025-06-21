9bf5f5071e0e24f3e41ed846ea49e676836158f3
MetaL Kir <masterzatk@gmail.com>
2025-06-22 00:40:03 +0300

    Lesson 16 homework

--- 16_AbstractClassInterface_18_06_2025/src/telran/employee/dao/CompanyImpl.java
+++ 16_AbstractClassInterface_18_06_2025/src/telran/employee/dao/CompanyImpl.java
@@ -1,6 +1,7 @@
 package telran.employee.dao;
 
 import telran.employee.model.Employee;
+import telran.employee.model.SalesManager;
 
 public class CompanyImpl implements Company {
     Employee[] employees;
@@ -12,28 +13,59 @@ public class CompanyImpl implements Company {
 
     @Override
     public boolean addEmployee(Employee employee) {
-        // TODO
-        return false;
+        if (size == employees.length)
+            return false;
+
+        for (int i = 0; i < size; i++) {
+            if (employees[i] == employee)
+                return false;
+        }
+
+        employees[size] = employee;
+        size++;
+        return true;
     }
 
     @Override
     public Employee removeEmployee(int id) {
+        for (int i = 0; i < size; i++) {
+            if(employees[i].getId() == id) {
+                Employee employeeToRemove = employees[i];
+                employees[i] = employees[size - 1];
+                employees[size - 1] = null;
+                size--;
+                return employeeToRemove;
+            }
+        }
         return null;
     }
 
     @Override
     public Employee findEmployee(int id) {
+        for (int i = 0; i < size; i++) {
+            if(employees[i].getId() == id)
+                return employees[i];
+        }
         return null;
     }
 
     @Override
     public double totalSalary() {
-        return 0;
+        double salaryTotal = 0;
+        for (int i = 0; i < size; i++) {
+            salaryTotal += employees[i].calcSalary();
+        }
+        return salaryTotal;
     }
 
     @Override
     public double averageSalary() {
-        return 0;
+        double salaryAverage = 0;
+        for (int i = 0; i < size; i++) {
+            salaryAverage += employees[i].calcSalary();
+        }
+        salaryAverage /= size;
+        return salaryAverage;
     }
 
     @Override
@@ -43,11 +75,18 @@ public class CompanyImpl implements Company {
 
     @Override
     public void printEmployees() {
-
+        for (int i = 0; i < size; i++) {
+            System.out.println(employees[i]);
+        }
     }
 
     @Override
     public double totalSales() {
-        return 0;
+        double salesSum = 0;
+        for (int i = 0; i < size; i++) {
+            if(employees[i] instanceof SalesManager seller)
+                salesSum += seller.getSalesValue();
+        }
+        return salesSum;
     }
 }
--- 16_AbstractClassInterface_18_06_2025/src/telran/employee/tests/CompanyTest.java
+++ 16_AbstractClassInterface_18_06_2025/src/telran/employee/tests/CompanyTest.java
@@ -9,6 +9,9 @@ import telran.employee.model.Manager;
 import telran.employee.model.SalesManager;
 import telran.employee.model.WageEmployee;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompanyTest {
@@ -17,7 +20,7 @@ class CompanyTest {
 
     @BeforeEach
     void setUp() {
-        company = new CompanyImpl(4);
+        company = new CompanyImpl(5);
         firm = new Employee[4]; // to check CompanyImpl
 
         firm[0] = new Manager(1000, "John", "Smith", 180, 20_000, 20);
@@ -33,39 +36,85 @@ class CompanyTest {
     @Test
     void addEmployee() {
         assertFalse(company.addEmployee(firm[3]));
-        Employee employee = new SalesManager(5000, "Andy", "Jackson", 90, 40_000, 0.1);
+        assertEquals(4, company.size());
+
+        Employee employee = new SalesManager(6666, "John", "Doe", 66, 66_666, 0.6);
         assertTrue(company.addEmployee(employee));
         assertEquals(5, company.size());
-        employee = new SalesManager(6000, "Andy", "Jackson", 90, 40_000, 0.1);
+
+        employee = new SalesManager(7777, "Johny", "Doe", 77, 77_777, 0.7);
         assertFalse(company.addEmployee(employee));
+        assertEquals(5, company.size());
     }
 
     @Test
     void removeEmployee() {
+        assertNull(company.removeEmployee(8888));
+        assertEquals(4, company.size());
+
+        assertEquals(firm[1], company.removeEmployee(firm[1].getId()));
+        assertEquals(3, company.size());
     }
 
     @Test
     void findEmployee() {
+        assertEquals(firm[1], company.findEmployee(firm[1].getId()));
+        assertNull(company.removeEmployee(8888));
     }
 
     @Test
     void totalSalary() {
+        double salary = 0;
+        for (Employee employee : firm){
+            salary += employee.calcSalary();
+        }
+
+        assertEquals(salary, company.totalSalary());
     }
 
     @Test
     void averageSalary() {
+        double salary = 0;
+        for (Employee employee : firm){
+            salary += employee.calcSalary();
+        }
+        salary /= firm.length;
+
+        assertEquals(salary, company.averageSalary());
     }
 
     @Test
     void size() {
         assertEquals(4, company.size());
+
+        company.removeEmployee(firm[1].getId());
+        assertEquals(3, company.size());
+
+        Employee employee = new SalesManager(6666, "John", "Doe", 66, 66_666, 0.6);
+        company.addEmployee(employee);
+        assertEquals(4, company.size());
     }
 
     @Test
     void printEmployees() {
+        PrintStream originalOut = System.out;
+
+        ByteArrayOutputStream outExpected = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outExpected));
+        for (Employee employee : firm)
+            System.out.println(employee);
+
+        ByteArrayOutputStream outActual = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outActual));
+        company.printEmployees();
+
+        System.setOut(originalOut);
+
+        assertEquals(outActual.toString(), outExpected.toString());
     }
 
     @Test
     void totalSales() {
+        assertEquals(600_000, company.totalSales());
     }
 }
